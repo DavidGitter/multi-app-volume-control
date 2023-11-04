0fb4902ae44388f6e239028e2bb4bfe68e07fb72
DavidGitter <80157809+DavidGitter@users.noreply.github.com>
2023-11-04 20:13:24 +0100

    service: implemented communication
    a simple and fast communication protocoll bestween the pc (target) and the hardware (mixer)

---
+++ mavc-target-agent/mavc-target-agent/COM.cs
@@ -0,0 +1,155 @@
+﻿using System.IO.Ports;
+
+/**
+ * A simple but fast communication class for stream-like commands bidirectional
+ */
+class COM
+{
+    //the serial port for com.
+    private SerialPort serialPort;
+
+    //stores the callback function for the receiver
+    private Action<Word> onReceiveFunc;
+
+    /**
+     * a class that expresses a word (command/data) in the com protocoll
+     */
+    public class Word
+    {
+        public Word(char action, String args)
+        {
+            this.action = action;
+            this.args = args;
+        }
+        public char action;
+        public String args;
+
+        public override string ToString()
+        {
+            return action + "," + args + "#";
+        }
+    }
+
+    /**
+     * @param portName  the name of the port (std: COM3)
+     * @param baudRate  the baud rate of the com. session
+     */
+    public COM(String portName = "COM3", int baudRate = 9600)
+    {
+        serialPort = new SerialPort(portName, baudRate);
+        serialPort.Open();
+        //set callback proxy function
+        serialPort.DataReceived += receivingCallbackProxy;
+    }
+
+    /**
+     * Returns the name of the port (e.g. "COM3")
+     */
+    public String GetPortName()
+    {
+        return serialPort.PortName;    
+    }
+
+    /**
+     * Returns the baud rate of the communication session
+     */
+    public int GetBaudRate()
+    {
+        return serialPort.BaudRate;
+    }
+
+    /*** ########## SENDER */
+
+     /**
+     * Sends a word by its two propeties
+     * 
+     * @param action    a letter that is related to a action or data (freely chooseable)
+     * @param args      the data in form of a string (formless)
+     */
+    void sendCommand(char action, String args) {
+        serialPort.Write(action + "," + args + "#");
+    }
+
+    /**
+    * Sends a word 
+    * 
+    * @param action    a letter that is related to a action or data (freely chooseable)
+    * @param args      the data in form of a string (formless)
+    */
+    void sendCommand(Word w)
+    {
+        serialPort.Write(w.ToString());
+    }
+
+    /*** ########## RECEIVER */
+
+    /**
+    * A callback funtion that gets triggered on data input
+    * 
+    * @param sender    the sender of the data
+    * @param e         additional event args
+    */
+    private void receivingCallbackProxy(object sender, SerialDataReceivedEventArgs e)
+    {
+        Console.WriteLine("Info: Received data stream");
+        while (serialPort.BytesToRead > 0)
+        {
+            char rbyte = (char)serialPort.ReadByte();
+            String word = "";
+            while (rbyte != '#')
+            {
+                word += rbyte;
+                rbyte = (char)serialPort.ReadByte();
+            }
+            try
+            {
+                Word w = extractWord(word);
+                onReceiveFunc(w);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    /**
+     * Function to set the callback function on word receiving event
+     * 
+     * @param wordInterpreter   the function that interprets and actions on the received word(s)
+     */
+    public void OnWordStreamReceive(Action<Word> wordInterpreter)
+    {
+        this.onReceiveFunc = wordInterpreter;
+    }
+
+    /**
+     * This function interprets a collected word and executes the action related to it
+     * 
+     * Example for a word: A,123#
+     * A word consists of 
+     *  - a char (action) that represents the action taken
+     *  - the delimiter ','
+     *  - a argument value for the action (volume in % as example)
+     *  - a # seperator that delimits it from the next word
+     *  
+     *  With this schema the protocoll can stream data
+     *  Example: A,45#B,54#U,2356#E,4353#.....
+     *  
+     *  @param word     the word to interpret
+     *  @note you can extend the function with new actions for new features
+     */
+    private Word extractWord(String word)
+    {
+        char action = word.ElementAt(0);   
+        char sep = word.ElementAt(1);
+        String arg = word.Substring(2);
+
+        if (!char.IsLetter(action))
+            throw new InvalidDataException();
+        if (!sep.Equals(','))
+            throw new InvalidDataException();
+
+        return new Word(action, arg);
+    }
+}
--- mavc-target-agent/mavc-target-agent/MavcAgent.cs
+++ mavc-target-agent/mavc-target-agent/MavcAgent.cs
@@ -1,7 +1,12 @@
-﻿
+﻿using static COM;
+
+
+/**
+ * Test enviroment of the target service/agent
+ */
 class MavcAgent
 {
-    static void Main(string[] args)
+    /*static void Main(string[] args)
     {
         AudioController controller = new AudioController();
         foreach (var device in controller.GetAudioDevices())
@@ -19,5 +24,57 @@ class MavcAgent
 
         AudioDevice defaultDevice = controller.GetDefaultAudioDevice();
         Console.WriteLine("__Default Output Device: " + defaultDevice + "__");
+    }*/
+
+    public static AudioController audioContr = new AudioController();
+
+    /**
+     * Function that interprets the words receiveds of the mixer
+     * 
+     * @param word  the word to be interpreted (see COM class)
+     */
+    public static void interpretWord(COM.Word word)
+    {
+        char action = word.action;
+        String arg = word.args;
+
+        switch (action)
+        {
+            case 'A':
+                {
+                    int argNum = int.Parse(arg);
+                    Console.WriteLine("Set audio -2");
+                    AudioDevice deflDev = audioContr.GetDefaultAudioDevice();
+                    deflDev.SetVolume(argNum);
+                    break;
+                }
+            case 'B':
+                {
+                    float argNum = int.Parse(arg);
+                    Console.WriteLine("Set audio: " + argNum);
+                    AudioDevice deflDev = audioContr.GetDefaultAudioDevice();
+                    deflDev.SetVolume(argNum/100);
+                    break;
+                }
+            default:
+                throw new InvalidDataException();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        while (true) {
+            try
+            {
+                COM comServer = new COM();
+                comServer.OnWordStreamReceive(MavcAgent.interpretWord);
+
+                Console.ReadLine();
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());    
+                Thread.Sleep(1000);
+            }
+        }
     }
 }
