d73058b25fc2ca0bae53df48c4b14652a4b9da2e
DavidGitter <80157809+DavidGitter@users.noreply.github.com>
2026-01-23 22:50:24 +0100

    added fix preventing duplicating mavc application

--- mavc-target-ui-win/mavc-target-ui-win/Program.cs
+++ mavc-target-ui-win/mavc-target-ui-win/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
+﻿using NAudio.Utils;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace mavc_target_ui_win
 {
+    static class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public const int SW_RESTORE = 9;
+
+        public static void NotifyExistingInstance()
+        {
+            var current = Process.GetCurrentProcess();
+            IntPtr hWnd = FindWindow(null, "MAVC");
+            if (hWnd != IntPtr.Zero)
+            {
+                ShowWindow(hWnd, SW_RESTORE);
+                SetForegroundWindow(hWnd);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+    }
+
     internal static class Program
     {
+
+        private static Mutex appRunningMutex;
 
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
@@ -17,6 +47,17 @@ namespace mavc_target_ui_win
         [STAThread]
         static void Main()
         {
+            //check if application already running, if so open the already existing
+            bool createdNew;
+            appRunningMutex = new Mutex(true, "MavcApplicationRunningMutex", out createdNew);
+
+            if (!createdNew)
+            {
+                // App läuft bereits
+                NativeMethods.NotifyExistingInstance();
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
