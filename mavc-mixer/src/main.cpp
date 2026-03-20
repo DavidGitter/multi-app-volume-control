4866d6a0f02b52b5c33613d0bb57a7b30a5bd52f
Thomas Klug <thomas_klug@gmx.de>
2026-03-20 22:28:42 +0100

    feat: send currentVol

--- mavc-mixer/src/main.cpp
+++ mavc-mixer/src/main.cpp
@@ -11,11 +11,13 @@ COMClient cc("COM3", 9600);
  *   ADC1 (safe with Wi-Fi): 32, 33, 34, 35, 36 (VP), 39 (VN)
  *   ADC2 (not safe with Wi-Fi): 0, 2, 4, 12, 13, 14, 15, 25, 26, 27
  */
-int potiPins[] = {36, 39, 34, 35, 32}; // Hardcoded for now
+int potiPins[] = { 36, 39, 34, 35, 32 }; // Hardcoded for now
 const int POTI_COUNT = sizeof(potiPins) / sizeof(potiPins[0]);
+const unsigned long transmissionDelay = 20;
 
 TemporalTresholdLock tta[POTI_COUNT];
-int lastVol[POTI_COUNT];
+int currentVol[POTI_COUNT]; // current filtered reading
+int lastVol[POTI_COUNT]; // last value already transmitted
 ADC* potis[POTI_COUNT];
 
 /**
@@ -70,20 +72,28 @@ void sendVolumes() {
  * Continuously reads potentiometer values and sends updates if changed
  */
 void loop() {
-  // initial states once
-  sendVolumes();
+  static bool initialStatesSent = false;
+  static unsigned long lastTransmitMs = 0;
 
-  while (true) {
+  if (!initialStatesSent) {
+    sendVolumes();
+    initialStatesSent = true;
+  }
+
+  for (int e = 0; e < POTI_COUNT; e++) {
+    currentVol[e] = potis[e]->getValue();   // filtered + hysteresis
+  }
+
+  if (millis() - lastTransmitMs >= transmissionDelay) {
+    lastTransmitMs = millis();
+
     for (int e = 0; e < POTI_COUNT; e++) {
-      int potValue = potis[e]->getValue();   // filtered + hysteresis
-      sendVolume(e, (char)(65 + e), potValue);
+      sendVolume(e, (char)(65 + e), currentVol[e]);
     }
 
     /* if (cc.receivedCommand()) {
       COMClient::Command c = cc.readCommand();
       onReceive(c);
     } */
-
-    delay(20);
   }
 }
